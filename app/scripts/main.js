f346407770cc14547313cf2344c0bf85c3d43fe3
Bradley Bossard <bradleybossard@gmail.com>
2014-07-24 17:33:15 -0700

    Adding initial code to download some json to be used in the timeline

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+  $.getJSON('../json/presidents.json', function(data) {
+    var container = document.getElementById('timeline-container');
+    var options = {};
+    var timeline = new vis.Timeline(container, data, options);
+    });
+});
