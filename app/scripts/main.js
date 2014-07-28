bc846eb11080163de945483ce15bb48b376e2aa9
Bradley Bossard <bradleybossard@gmail.com>
2014-07-27 17:02:22 -0700

    Massage data in Open Refine to create initial working json file. Changes to JS to take the presidents.json data and put it in a format that vis.js likes.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
   $.getJSON('../json/presidents.json', function(data) {
+    var timelineData = [];
+    $.each(data.presidents, function(i, president) {
+      var timelineEl = {};
+      timelineEl.id = president.office_order;
+      timelineEl.content = president.name;
+      timelineEl.start = president.birth_date;
+      if (president.death_date != null) { 
+        timelineEl.end = president.death_date;
+      }
+      timelineData.push(timelineEl);
+    });
+
     var container = document.getElementById('timeline-container');
     var options = {};
-    var timeline = new vis.Timeline(container, data, options);
+    var timeline = new vis.Timeline(container, timelineData, options);
     });
 });
